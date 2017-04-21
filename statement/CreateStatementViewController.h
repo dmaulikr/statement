138f0bb09c3e3552212cc2cdf47afa693abb4d9c
KuharAC <kuharac@gmail.com>
2017-04-21 12:47:10 -0500

    feat: Add TableViewDelegate and TableViewDataSource functionality
    createStatement functionality will not add new statements to the tableView. As a result, statementLabel
    has been removed and Statements are not displayed entirely in the tableView.

--- statement/CreateStatementViewController.h
+++ statement/CreateStatementViewController.h
@@ -10,14 +10,13 @@
 #import "AppDelegate.h"
 #import "Statement+CoreDataClass.h"
 
-@interface CreateStatementViewController : UIViewController <UITextFieldDelegate, NSFetchedResultsControllerDelegate>
+@interface CreateStatementViewController : UIViewController <UITextFieldDelegate, NSFetchedResultsControllerDelegate, UITableViewDelegate, UITableViewDataSource>
 
-@property (weak, nonatomic) IBOutlet UILabel *statementLabel;
 @property (weak, nonatomic) IBOutlet UITextField *statementTextField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (nonatomic, retain) NSFetchedResultsController *fetchController;
 @property (nonatomic, retain) NSManagedObjectContext *context;
-
 @property (strong, nonatomic) Statement *createdStatement;
 
 - (IBAction)createStatement:(UITextField *)sender;
--- statement/CreateStatementViewController.m
+++ statement/CreateStatementViewController.m
@@ -14,52 +14,75 @@
 
 @implementation CreateStatementViewController
 
+#pragma mark - UIViewController
+
 @synthesize createdStatement;
 
 AppDelegate *appDelegate;
-Statement *fetchedStatement;
+NSMutableArray *statementArray;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     _statementTextField.delegate = self;
+    
     appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     _context = [[appDelegate persistentContainer] viewContext];
     
     _fetchController = [appDelegate initializeFetchedResultsController];
-    fetchedStatement = (Statement *)_fetchController.fetchedObjects.firstObject;
     
-    NSLog(@"%@", fetchedStatement.statementText);
-    
-    _statementLabel.text = fetchedStatement.statementText;
-    
+    statementArray = [_fetchController.fetchedObjects mutableCopy];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     
-    
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [appDelegate saveContext];
 }
 
 - (IBAction)createStatement:(UITextField *)sender {
     
-    for (NSManagedObject *object in _fetchController.fetchedObjects) {
+    /*for (NSManagedObject *object in _fetchController.fetchedObjects) {
+        
      [_fetchController.managedObjectContext deleteObject:object];
-    }
-    
-    _statementLabel.text = _statementTextField.text;
-    NSLog(@"%@", _statementLabel.text);
-    
+    }*/
     
     self.createdStatement = [NSEntityDescription insertNewObjectForEntityForName:@"Statement" inManagedObjectContext:_context];
-    self.createdStatement.statementText = _statementLabel.text;
+    self.createdStatement.statementText = _statementTextField.text;
+    
+    [statementArray addObject:createdStatement];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[statementArray indexOfObject:createdStatement] inSection:0];
+    
+    [_tableView beginUpdates];
+    [_tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];
+    [_tableView endUpdates];
     
     [appDelegate saveContext];
 }
+
+#pragma mark - TableView Delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    if (statementArray == nil) {
+        return 0;
+    } else {
+        return [statementArray count];
+    }
+}
+
+#pragma mark - TableView Data Source
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"statementCell"];
+    Statement *statementIndexObject = (Statement *)[statementArray objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = statementIndexObject.statementText;
+    
+    return cell;
+}
+
+#pragma mark - TextField Delegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
