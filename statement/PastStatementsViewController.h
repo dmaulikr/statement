ec25b58605e2b299960ddafea6570c1debbdcf9d
KuharAC <kuharac@gmail.com>
2017-06-19 14:48:09 -0500

    feat: Add PastStatementsVC files and a matching Storyboard scene

---
+++ statement/PastStatementsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PastStatementsViewController.h
+//  statement
+//
+//  Created by Alexander Kuhar on 6/19/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PastStatementsViewController : UIViewController
+
+@end
