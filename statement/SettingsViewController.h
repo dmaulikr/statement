87d44cddfdb04002d6c6dcce613c5dc5ab2f0259
KuharAC <kuharac@gmail.com>
2017-06-22 16:15:09 -0500

    feat: Add SettingsVC files and its Storyboard scene

---
+++ statement/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  statement
+//
+//  Created by Alexander Kuhar on 6/22/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
