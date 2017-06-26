8202c1de0ca423bc8ee4a5b50795d572fb3a502c
KuharAC <kuharac@gmail.com>
2017-06-26 12:13:28 -0500

    feat: Add custom UITableViewCell: EnableNotificationsCell

---
+++ statement/EnableNotificationsCell.h
@@ -0,0 +1,16 @@
+//
+//  EnableNotificationsCell.h
+//  statement
+//
+//  Created by Alexander Kuhar on 6/23/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface EnableNotificationsCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIButton *enableNotificationsButton;
+
+@end
---
+++ statement/EnableNotificationsCell.m
@@ -0,0 +1,26 @@
+//
+//  EnableNotificationsCell.m
+//  statement
+//
+//  Created by Alexander Kuhar on 6/23/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import "EnableNotificationsCell.h"
+
+@implementation EnableNotificationsCell
+
+@synthesize enableNotificationsButton;
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
