594568777816bfbd003a310a1e06712af7242e49
KuharAC <kuharac@gmail.com>
2017-06-28 11:52:25 -0500

    feat: Add PastStatementDetailsVC and a corresponding Storyboard

---
+++ statement/PastStatementDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PastStatementDetailsViewController.h
+//  statement
+//
+//  Created by Alexander Kuhar on 6/28/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PastStatementDetailsViewController : UIViewController
+
+@end
