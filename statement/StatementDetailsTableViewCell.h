9dba2bb3273a9655b11d669b2da314746c02e3af
KuharAC <kuharac@gmail.com>
2017-06-29 12:51:56 -0500

    feat: Add custom cell StatementDetailsTableViewCell

---
+++ statement/StatementDetailsTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  StatementDetailsTableViewCell.h
+//  statement
+//
+//  Created by Alexander Kuhar on 6/29/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatementDetailsTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *detailsLabel;
+
+@end
---
+++ statement/StatementDetailsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  StatementDetailsTableViewCell.m
+//  statement
+//
+//  Created by Alexander Kuhar on 6/29/17.
+//  Copyright © 2017 Alexander Kuhar. All rights reserved.
+//
+
+#import "StatementDetailsTableViewCell.h"
+
+@implementation StatementDetailsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
